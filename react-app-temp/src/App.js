d6c3be1c055f5a1bd37e7460e19a36946a4c501e
pepea <pepea.23@live.com>
2019-12-21 16:20:54 +0700

    :sparkles: add link to homepage :school_satchel:

--- react-app-temp/src/App.js
+++ react-app-temp/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
 import Homepage from './components/template/homepage'
+import './utils/GlobalStyle.css'
 
 export default function App() {
   return (
--- react-app-temp/src/components/template/homepage/index.js
+++ react-app-temp/src/components/template/homepage/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const HomePage = () => {
-  return <div>Homepage</div>
+  return (
+    <div className='homepage'>
+      <p className='color-green'>HomePage</p>
+      <p className='color-green'>Hi this is free template by Zompong</p>
+    </div>
+  )
 }
 
 export default HomePage
